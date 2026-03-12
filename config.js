69e6cd2e08234124d220aa2af513f92558a53e90
danny700808 <danny700808@gmail.com>
2026-03-12 20:41:47 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-https://script.google.com/macros/s/AKfycbxB0Z0OSox6qikxiWmqKPiD50zizkfYiR4y5kekfCSFFt91nQfNd8hWafRkemG0Xfk/exec
+const API_URL = "https://script.google.com/macros/s/AKfycbxB0Z0OSox6qikxiWmqKPiD50zizkfYiR4y5kekfCSFFt91nQfNd8hWafRkemG0Xfk/exec";
