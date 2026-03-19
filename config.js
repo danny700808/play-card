3728d108388e395e38d2bf1b1fb6ad0422986e5b
danny700808 <danny700808@gmail.com>
2026-03-20 01:31:16 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
   API_URL: 'https://script.google.com/macros/s/AKfycby2fAM3Q5j9-4je9atMNzbdNIXn3-Y90nacN75jJgCyO7fSglgBR9iE-lOEacmh7dI_/exec',
-  GOOGLE_CLIENT_ID: ''
+  GOOGLE_CLIENT_ID: '1061451682971-664fkp8jnd3771srdrshujqalngnocla.apps.googleusercontent.com'
 };
