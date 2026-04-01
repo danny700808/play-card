4d658da348a523a0d124b3adb0fa94fd78bad19e
danny700808 <danny700808@gmail.com>
2026-04-01 23:24:56 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.APP_CONFIG = {
   API_URL: 'https://script.google.com/macros/s/AKfycby2fAM3Q5j9-4je9atMNzbdNIXn3-Y90nacN75jJgCyO7fSglgBR9iE-lOEacmh7dI_/exec',
   GOOGLE_CLIENT_ID: '1061451682971-664fkp8jnd3771srdrshujqalngnocla.apps.googleusercontent.com',
   CLOUDINARY_CLOUD_NAME: 'dkwzybiw9',
-  CLOUDINARY_UPLOAD_PRESET: 'yuzu_unsigned',
+  CLOUDINARY_UPLOAD_PRESET: 'yuzu_unsigned2',
   CLOUDINARY_ROOT_FOLDER: 'employee-system',
   CLOUDINARY_CHUNK_SIZE_MB: 20,
   CLOUDINARY_SOFT_MAX_VIDEO_MB: 0
