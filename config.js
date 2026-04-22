89a03488a47a8b9b0b010a746174fc54687727c7
danny700808 <danny700808@gmail.com>
2026-04-22 22:58:19 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,13 +1,14 @@
 (function(global){
   const DEFAULTS = {
-    API_URL: 'https://script.google.com/macros/s/AKfycby2fAM3Q5j9-4je9atMNzbdNIXn3-Y90nacN75jJgCyO7fSglgBR9iE-lOEacmh7dI_/exec',
+    // 這裡請在你部署新 Apps Script Web App 後，改成新的 /exec 網址
+    API_URL: 'https://請貼上你的新WebApp網址/exec',
     GOOGLE_CLIENT_ID: '1061451682971-664fkp8jnd3771srdrshujqalngnocla.apps.googleusercontent.com',
     CLOUDINARY_CLOUD_NAME: 'dkwzybiw9',
     CLOUDINARY_UPLOAD_PRESET: 'yuzu_unsigned2',
     CLOUDINARY_ROOT_FOLDER: 'employee-system',
     CLOUDINARY_CHUNK_SIZE_MB: 20,
     CLOUDINARY_SOFT_MAX_VIDEO_MB: 0,
-    BUILD: '2026-04-22-notify-center-clean-v1'
+    BUILD: '2026-04-22-new-project-notify-center-v1'
   };
 
   const params = new URLSearchParams(global.location.search || '');
